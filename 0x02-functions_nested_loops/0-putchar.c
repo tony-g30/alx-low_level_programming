fc7ba25e72ee785b21f16aae1ef93319c54f3850
Tony Audo <tonyaudo@gmail.com>
2022-09-14 22:39:36 +0300

    task 0 correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /*
- *return: always 0 if program successful
- *main function: entry into program
+ *return:always 0 if program successful
+ *main:entry into program
  */
 int main(void)
 {
