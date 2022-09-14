4e0528a24d6939640b0d67c72da162523d90ee12
Tony Audo <tonyaudo@gmail.com>
2022-09-14 23:17:31 +0300

    task 1 correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+void print_alphabet(void)
+
 /*
+ * main - calls the function to excute
  *print_alphabet - prints alphabet in lowercase
  *return -always successful
  */
-void print_alphabet()
+int main()
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)void print_alphabet(void)
+void print_alphabet(void)
 {
         char i;
 
