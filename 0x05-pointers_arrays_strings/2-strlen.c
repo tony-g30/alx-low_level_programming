89cf8d747f257f4b579a33449e2e74d02a7e7045
Tony Audo <tonyaudo@gmail.com>
2022-09-20 02:14:48 +0300

    correction

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include<string.h>
 /**
- *_strlen -> calculates string length
- *return: integer c 
- *@s: points to str
- */
+*_strlen -> calculates string length
+*return:integer c
+*@s:points to str
+*/
 int _strlen(char *s)
 {
 	int c = 0;
