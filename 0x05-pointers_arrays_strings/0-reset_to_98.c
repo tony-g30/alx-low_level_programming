0ad502c06797b9dc84b03e880cd1839a6b28d06d
Tony Audo <tonyaudo@gmail.com>
2022-09-19 21:43:28 +0300

    dereferencing correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - 
- * reset_to_98 - uses pointer to update variable
- * return: always 0
+ *main
+ *reset_to_98 -> uses pointer to update variable
+ *return: always 0
  */
 void reset_to_98(int *n)
 {
