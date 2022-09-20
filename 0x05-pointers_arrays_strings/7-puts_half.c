2fa416ba32331675f8f34bd6f545794ba1ab98da
Tony Audo <tonyaudo@gmail.com>
2022-09-20 21:50:08 +0300

    printing half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *puts_half - prints last half of string
+ *@str: the string to be printed
+ *return: void
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int n, a, b;
+
+	n = 0;
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+
+	if (n % 2 == 1)
+	{
+		a = n - 1 / 2;
+		a += 1;
+	}
+	else
+	{
+		a = n / 2;
+	}
+
+	for (; a < n; a++)
+	{
+		putchar(str[a]);
+	}
+}
