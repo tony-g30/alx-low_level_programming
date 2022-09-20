bf100a5cfc0d9f6314d091380b189dcc4ac547d1
Tony Audo <tonyaudo@gmail.com>
2022-09-20 16:43:48 +0300

    corrections

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string
+ *@s: string to be modified
+ *
+ *return - void
+ */
+void rev_string(char *s)
+{
+	char *a, x;
+	int c = 0, k, i;
+
+	a = s;
+
+	while (s[c] != \0)
+		c++;
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+	for (i = 0; i < (c / 2); i++)
+	{
+		x = s[i];
+		s[i] = *a;
+		*a = x;
+		a--;
+	}
+}
