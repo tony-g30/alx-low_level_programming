dbfd1a55edd393b720a898d600a7bcb148730a7c
Tony Audo <tonyaudo@gmail.com>
2022-09-22 10:30:17 +0300

    strncpy corrections

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
-*_strncpy -> copoes an inputted no. od bytes from src to dest 
+*_strncpy -> copoes an inputted no. od bytes from src to dest
 *@dest: buffer storing string copy
 *@src: source string
 *@n: minimum no. of bytes to be copied from source string
+*Return: character value
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, slen = 0;
-	
+
 	while (src[slen])
 		slen++;
 
-	while (a < n && src[i])
+	while (i < n && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (a < n)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
