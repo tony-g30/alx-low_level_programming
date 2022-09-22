365c145016c59616985d4f07b763801983274d01
Tony Audo <tonyaudo@gmail.com>
2022-09-22 09:35:22 +0300

    capitalising all

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*cap_string
+*
+*
+*
+*/
+char *cap_string(char *)
+{
+	int a = 0, i, cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{'. '}'}'
+
+	while (s[a])
+	{
+		i = 0;
+		
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32
+			i++;
+		}
+		a++;
+	}
+	return (s);
+}
