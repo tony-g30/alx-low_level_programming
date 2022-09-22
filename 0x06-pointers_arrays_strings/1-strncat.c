d73c032d7f458c86c51794b7e5cf4a537343c17c
Tony Audo <tonyaudo@gmail.com>
2022-09-22 08:01:26 +0300

    fixed errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*_strcat -> this function concatenates two strings
+*_strncat -> this function concatenates two strings
 *@dest: first parameter
 *@src: second parameter
-*return: a pointer to resulting dest string
 *@n: number of bytes of src to be appended to dest
+**return: a pointer to resulting dest string
 */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -20,5 +20,4 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	dest[dlen + n + 1] = '\0';
 	return (dest);
-}
-	
+}	
