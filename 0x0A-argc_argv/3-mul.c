0d54d54847b22bd05611e9edc85e40c8e3d5c017
Tony Audo <tonyaudo@gmail.com>
2022-10-01 03:30:08 +0300

    hot fix

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main -> this is a function to print the result of multiplication operation
+*@argc: represents no. of arguments
+*@argv: contains array of the arguments
+*Return: returns 0 if successful
+*/
+int main(int argc, char *argv)
+{
+	int a, b, res;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	a = atoi(argc[1]);
+	b = atoi(argv[2]);
+
+	res = a * b;
+	printf("%d\n", res);
+
+	return (0);
+}
